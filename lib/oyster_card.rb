7efd2d4ec23c5611d3fdc077360d2189fc12d0fd
Royston <rlqhenson@hotmail.com>
2016-10-03 22:34:32 +0100

    step: enable top up functionality

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -4,4 +4,8 @@ attr_reader :balance
   def initialize
       @balance = 0
   end
+
+  def top_up(amount)
+    @balance += amount
+  end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -5,4 +5,13 @@ describe OysterCard do
   it "has a default balance of 0" do
     expect(subject.balance).to eq(0)
   end
+  context '#top_up' do
+    it "responds to top_up" do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+    it "tops up card" do
+      subject.top_up(10)
+      expect(subject.balance).to eq(10)
+    end
+  end
 end
