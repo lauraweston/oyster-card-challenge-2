6f72f32cc9349201382095db8c6dc1495d31a447
Ed Powderham <edpowderham@outlook.com>
2016-10-04 10:28:34 +0100

    Added touch in, touch out and in journey? methods

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,26 +1,42 @@
 class OysterCard
-attr_reader :balance
+attr_reader :balance, :in_journey
 
 MAX_LIMIT = 90
 
   def initialize
       @balance = 0
+      @in_journey = false
   end
 
   def top_up(amount)
-    raise max_limit if (@balance + amount) > MAX_LIMIT
+    raise max_limit if limit_reached?(@balance + amount)
     @balance += amount
-    @amount = amount
   end
 
   def deduct(amount)
     @balance -= amount
   end
 
+  def in_journey?
+    in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
   private
 
   def max_limit
     "The maximum amount allowed on the card is £#{MAX_LIMIT}"
+  end
+
+  def limit_reached?(amount)
+    (amount) > MAX_LIMIT
   end
 
 
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,32 +1,58 @@
 require "oyster_card"
 
 describe OysterCard do
-  subject {OysterCard.new}
+  subject(:oystercard) {described_class.new}
+
   it "has a default balance of 0" do
-    expect(subject.balance).to eq(0)
+    expect(oystercard.balance).to eq(0)
   end
+
   context "#top_up" do
     it "responds to top_up" do
-      expect(subject).to respond_to(:top_up).with(1).argument
+      expect(oystercard).to respond_to(:top_up).with(1).argument
     end
+
     it "tops up card" do
-      subject.top_up(10)
-      expect(subject.balance).to eq(10)
+      oystercard.top_up(10)
+      expect(oystercard.balance).to eq(10)
     end
+
     it "prevents top up above 90" do
       MAX_LIMIT = 90
-      subject.top_up(90)
-      expect{subject.top_up(1)}.to raise_error("The maximum amount allowed on the card is £90")
+      oystercard.top_up(90)
+      expect{oystercard.top_up(1)}.to raise_error("The maximum amount allowed on the card is £90")
     end
   end
+
   context "#deduct" do
     it "responds to deduct" do
-      expect(subject).to respond_to(:deduct).with(1).argument
+      expect(oystercard).to respond_to(:deduct).with(1).argument
     end
+
     it "deducts fare" do
-      subject.top_up(20) # replace with stub
-      subject.deduct(10)
-      expect(subject.balance).to eq(10)
+      oystercard.top_up(20) # replace with stub
+      oystercard.deduct(10)
+      expect(oystercard.balance).to eq(10)
     end
   end
+
+  context "#in_journey?" do
+    it "Initialized not in a journey" do
+      expect(oystercard.in_journey?).to eq false
+    end
+  end
+
+  context "#touch_in" do
+    it "Touching in changes in_journey variable to true" do
+      oystercard.touch_in
+      expect(oystercard.in_journey?).to eq true
+    end
+  end
+
+  context "#touch_out"
+    it "Touching out changes in_journey variable to false" do
+      oystercard.touch_in
+      oystercard.touch_out
+      expect(oystercard.in_journey?).to eq false
+    end
 end
